31ea070dfbeca9ff7803206173ebe4d4915894c2
Anderson Grudtner Martins <anderson@alledia.com>
2019-10-30 18:39:12 -0300

    Update the version number

--- EDD_SL_Plugin_Updater.php
+++ EDD_SL_Plugin_Updater.php
@@ -11,7 +11,7 @@ if ( ! class_exists('Alledia\\EDD_SL_Plugin_Updater')) {
  * Allows plugins to use their own update API.
  *
  * @author  Easy Digital Downloads
- * @version 1.6.16.1
+ * @version 1.6.16.2
  */
 class EDD_SL_Plugin_Updater
 {
